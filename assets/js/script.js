487fd63e1f6e0b9e9617b5704d10fbdcf6cec04a
Jonathan Bejarano <jbejar86@gmail.com>
2023-11-22 17:50:42 -0700

    adding map

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,9 @@ function getApi() {
 }
 
 fetchButton.addEventListener('click', getApi);
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
